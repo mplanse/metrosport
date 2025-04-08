5685f98cfeab0fc0ce9c0dd482eac03b16719894
ainhoablanco <ablancoe2324@politecnics.barcelona>
2025-04-08 16:14:31 +0200

    changes notis i lliga

--- metrosport/resources/views/notificacions/index.blade.php
+++ metrosport/resources/views/notificacions/index.blade.php
@@ -1,33 +1,34 @@
 @extends('layouts.chat')
 
 @section('title')
- · Notificacions
+    · Notificacions
 @endsection
 
 @section('content')
-<div class="container mt-5">
-    <h1 class="mb-4">Notificacions</h1>
+    <div class="container mt-5">
+        <h1 class="mb-4">Notificacions</h1>
 
-    @if($notificacions->isEmpty())
-        <div class="alert alert-warning text-center" role="alert">
-            No hi ha notificacions.
-        </div>
-    @else
-        <div class="list-group">
-            @foreach($notificacions as $notificacio)
-                <div class="list-group-item d-flex justify-content-between align-items-start border-0 rounded-3 shadow-sm mb-3">
-                    <div class="notification-content">
-                        <p class="mb-1 text-wrap">{{ $notificacio->missatge_notificacio }}</p>
+        @if ($notificacions->isEmpty())
+            <div class="alert alert-warning text-center" role="alert">
+                No hi ha notificacions.
+            </div>
+        @else
+            <div class="list-group">
+                @foreach ($notificacions as $notificacio)
+                    <div class="notification-card">
+                        <div class="notification-content">
+                            <p class="mb-1">{{ $notificacio->missatge_notificacio }}</p>
+                        </div>
+                        <div class="notification-footer">
+                            <small>
+                                <strong>Data:</strong> {{ \Carbon\Carbon::parse($notificacio->timestamp)->format('d/m/Y') }}
+                                <br>
+                                <strong>Hora:</strong> {{ \Carbon\Carbon::parse($notificacio->timestamp)->format('H:i') }}
+                            </small>
+                        </div>
                     </div>
-                    <div class="notification-footer text-end">
-                        <small class="text-muted">
-                            <strong>Data:</strong> {{ \Carbon\Carbon::parse($notificacio->timestamp)->format('d/m/Y') }} <br>
-                            <strong>Hora:</strong> {{ \Carbon\Carbon::parse($notificacio->timestamp)->format('H:i') }}
-                        </small>
-                    </div>
-                </div>
-            @endforeach
-        </div>
-    @endif
-</div>
+                @endforeach
+            </div>
+        @endif
+    </div>
 @endsection
